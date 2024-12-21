1e04e8718454476a02a0eaf4d83bd5a8bf61329c
DANIIL CHATZIIOANNIDIS <167763180+danxatzi@users.noreply.github.com>
2024-12-21 20:15:54 +0200

    hope its final

--- game.php
+++ game.php
@@ -8,6 +8,12 @@ $username = 'root';
 $password = '';
 
 
+$mysqli = new mysqli($host, $username, $password, $dbname,null,'/home/student/iee/2021/iee2021233/mysql/run/mysql.sock');
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . 
+    $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
