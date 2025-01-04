a172cbcd0dd3085eb1d5e3e8e004b1fce90b7198
DANIIL CHATZIIOANNIDIS <167763180+danxatzi@users.noreply.github.com>
2025-01-04 12:15:44 +0200

    sh

--- dbconnect.php
+++ dbconnect.php
@@ -3,11 +3,15 @@ $username='root';
 $password='';
 $host='localhost';
 $dbname = 'ataxx_game';
+$socket = '/home/student/iee/2021/iee2021233/mysql/run/mysql.sock';
 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;unix_socket=$socket", $username, $password);
+    
 
-$mysqli = new mysqli($host, $username, $password, $dbname,null,'/home/student/iee/2021/iee2021233/mysql/run/mysql.sock');
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . 
-    $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+
+    echo json_encode(['error' => 'Database connection failed: ' . $e->getMessage()]);
+    exit;
 }
-?>
