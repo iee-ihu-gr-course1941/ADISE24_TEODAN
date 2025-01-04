f2406389a911876d5d2ffbd1a23c72ec783f0cdd
DANIIL CHATZIIOANNIDIS <167763180+danxatzi@users.noreply.github.com>
2025-01-04 11:53:21 +0200

    sh

--- game.php
+++ game.php
@@ -12,7 +12,7 @@ require_once "dbconnect.php";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
 } catch (PDOException $e) {
     echo json_encode(['error' => 'Database connection failed: ' . $e->getMessage()]);
     exit;
