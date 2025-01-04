f8d4c3a3b4f57a3b020d61a483b2eaff7fcfcf33
DANIIL CHATZIIOANNIDIS <167763180+danxatzi@users.noreply.github.com>
2025-01-04 13:45:33 +0200

    sfdgsgf

--- dbconnect.php
+++ dbconnect.php
@@ -2,11 +2,11 @@
 $username='root';
 $password='';
 $host='localhost';
-$dbname = 'ataxx_game';
+$dbname = 'test_project_db';
 $socket = '/home/student/iee/2021/iee2021233/mysql/run/mysql.sock';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;unix_socket=$socket", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;socket=$socket", $username, $password);
     
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
