f3817d7d73d4d51cc5dec2858acf27b65f43a64f
DANIIL CHATZIIOANNIDIS <167763180+danxatzi@users.noreply.github.com>
2025-01-04 11:15:04 +0200

    gqawsfdhdf

--- game.php
+++ game.php
@@ -6,10 +6,10 @@ error_reporting(E_ALL);
 
 
 
-
-
-
 require_once "dbconnect.php";
+
+
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
