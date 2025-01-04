a88b15bea23624b2f4672e8a84f22c006dd6693e
DANIIL CHATZIIOANNIDIS <167763180+danxatzi@users.noreply.github.com>
2025-01-04 10:35:01 +0200

    waeighuloprhioluwgressjbd

--- dbconnect.php
+++ dbconnect.php
@@ -5,7 +5,7 @@ $host='localhost';
 $dbname = 'ataxx_game';
 
 
-$mysqli = new mysqli($host, $username, $password, $dbname);
+$mysqli = new mysqli($host, $username, $password, $dbname,null,'/run/mysqld/mysqld.sock');
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . 
     $mysqli->connect_errno . ") " . $mysqli->connect_error;
